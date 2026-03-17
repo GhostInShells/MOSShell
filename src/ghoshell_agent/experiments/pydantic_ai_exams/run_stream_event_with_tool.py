7925a2a3c05bb881de08361258b39c47a57d769a
thirdgerb <thirdgerb@gmail.com>
2026-03-17 22:01:46 +0800

    dev: initialize ghost agent

---
+++ src/ghoshell_agent/experiments/pydantic_ai_exams/run_stream_event_with_tool.py
@@ -0,0 +1,44 @@
+import asyncio
+from pydantic_ai import Agent
+from pydantic_ai.models.anthropic import AnthropicModel
+from pydantic_ai.providers.anthropic import AnthropicProvider
+
+
+# 假设 Container 已定义
+class Container:
+    pass
+
+
+# 注意：deepseek-reasoner 包含“思考过程”，目前部分 Provider 封装可能还在适配其 reasoning_content
+model = AnthropicModel(
+    'deepseek-reasoner',
+    provider=AnthropicProvider()
+)
+
+agent = Agent(model, deps_type=Container)
+container = Container()
+
+
+@agent.tool_plain()
+async def ctml_run(ctml: str) -> None:
+    """
+    接受一个 ctml 字符串.
+    """
+    print("++++++++++", ctml)
+
+
+async def run():
+    # 1. 启动流式运行
+    async for event in agent.run_stream_events(
+            "请你在思考中调用一次 ctml_run, 传入一个随机字符串, 然后在最终回复里也这么做", deps=container):
+        print("--- 开始接收流式输出 ---")
+
+        # 模式 A: 获取纯文本增量 (最常用)
+        # debounce_by=None 确保每个 token 立即输出，降低感知延时
+        print(f"Content Block event: {event!r}")
+
+        print("\n--- 流式结束 ---")
+
+
+if __name__ == "__main__":
+    asyncio.run(run())
