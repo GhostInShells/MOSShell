b9870873c12a0a7e86a3eb425f42cc5b6163269b
thirdgerb <thirdgerb@gmail.com>
2026-03-29 14:24:19 +0800

    dev: add environment for moss

--- src/ghoshell_moss/channel_interfaces/README.md
+++
@@ -1,3 +0,0 @@
-# Channel Interfaces
-
-这里放 MOSS 架构下一个 Agent 可能必要的功能或者范式级能力的抽象设计. 为具体实现做参考.
--- src/ghoshell_moss/channel_interfaces/module.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from ghoshell_moss.core.concepts.channel import ChannelInterface
-
-
-class ModuleChannel(ChannelInterface, ABC):
-    """
-    定义一种特殊的, 可以
-    """
